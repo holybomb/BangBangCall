343070cf9f7d580f64e1059e1f631357a69cd3e4
holybomb <holybomb@163.com>
2013-10-16 23:45:04 +0800

    + add proj

---
+++ src/com/dfrc/bangbangcall/screens/ScreenHome.java
@@ -0,0 +1,11 @@
+package com.dfrc.bangbangcall.screens;
+
+
+public class ScreenHome extends BaseScreen{
+
+	public ScreenHome(SCREEN_TYPE type, String id) {
+		super(type, id);
+		// TODO Auto-generated constructor stub
+	}
+
+}
